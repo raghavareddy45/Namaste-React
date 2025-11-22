f4583223675877cc0987f68926dbf4d2977194be
raghavareddy45 <rajaraghavareddy45@gmail.com>
2025-11-23 00:06:33 +0530

    added jsx

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-// const header=document.createElement("h1");
-// header.innerHTML="USING JS";
-// const root=document.getElementById("root");
-// root.appendChild(header);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const header=React.createElement("h1",{
-    id:"header",
-    class:"Main _ header"
-},"Haii Using React");
+// const header=React.createElement("h1",{
+//     id:"header",
+//     class:"Main _ header"
+// },"Haii Using React");
+const header=<h1>From Jsx</h1>;
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(header);
